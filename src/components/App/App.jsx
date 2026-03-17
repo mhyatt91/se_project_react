15e60265d1270ce1f476d425c41856424cc8accd
Missy <mhyatt91@yahoo.com>
2026-03-16 20:43:18 -0400

    blue card css and temperature fixed

---
+++ src/components/App/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import Header from "../Header/Header";
+import Main from "../Main/Main";
+import { useState } from "react";
+
+function App() {
+  const [weatherData, setWeatherData] = useState({ type: "hot" });
+
+  return (
+    <div className="page">
+      <div className="page__content">
+        <Header />
+        <Main weatherData={weatherData} />
+      </div>{" "}
+    </div>
+  );
+}
+
+export default App;
