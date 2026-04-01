0e7bbdac5ed9cb7b043e84dade792a1a7040b11b
Missy <mhyatt91@yahoo.com>
2026-04-01 14:57:42 -0400

    final project first submission

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import "./Footer.css";
 function Footer() {
   return (
     <footer class="footer">
-      <p class="footer__copyright">Developed by Missy H 2026 PEWPEPWPEWP</p>
+      <p class="footer__copyright">Developed by Missy H 2026</p>
     </footer>
   );
 }
