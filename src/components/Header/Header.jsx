2c98100abda57a9263682eecf59eac915aaf07fa
Missy <mhyatt91@yahoo.com>
2026-04-11 11:12:42 -0400

    Project 11 Submission 1

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,7 +11,9 @@ function Header({ handleAddClick, weatherData }) {
   });
   return (
     <header className="header">
-      <img className="header__logo" src={logo} alt="App logo" />
+      <Link to="/">
+        <img className="header__logo" src={logo} alt="App logo" />
+      </Link>
       <p className="header__date-and-location">
         {currentDate}, {weatherData.city}
       </p>
